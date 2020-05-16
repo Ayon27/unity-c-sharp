586edf1bf51e08142b53dc7075ec275904c1a3e4
ayon27 <ayon8295@gmail.com>
2020-05-17 00:33:44 +0600

    initial

---
+++ New Unity Project/Assets/Scripts/Talker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Talker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Im trash");
+    }
+
+    // Update is called once per frame
+}
